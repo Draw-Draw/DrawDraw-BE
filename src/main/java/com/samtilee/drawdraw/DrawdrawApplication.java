f09af0703237f2381ec9fe83e804202aff6674ff
Chan531 <chan657312@gmail.com>
2024-03-19 11:31:21 +0900

    초기 세팅

---
+++ src/main/java/com/samtilee/drawdraw/DrawdrawApplication.java
@@ -0,0 +1,13 @@
+package com.samtilee.drawdraw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrawdrawApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrawdrawApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/samtilee/drawdraw/DrawdrawApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samtilee.drawdraw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrawdrawApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
