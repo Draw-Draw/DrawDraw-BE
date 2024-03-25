6b70ba9356b9dc5cb635a9526f8d31958b6c4a13
Chan531 <chan657312@gmail.com>
2024-03-25 19:21:40 +0900

    [INIT] 컨트롤러 초기 세팅

---
+++ src/main/java/com/samtilee/drawdraw/diaryBook/controller/DiaryBookController.java
@@ -0,0 +1,11 @@
+package com.samtilee.drawdraw.diaryBook.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/diary-books")
+public class DiaryBookController {
+}
